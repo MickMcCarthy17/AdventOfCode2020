c2f281e47c724696f11dc5e3804adceb17fb5909
Louis Cowell <tyrone.cowell@tesco.com>
2020-12-02 18:24:58 +0000

    Part 2 - Find three numbers which sum to 2020 and multiply them

--- src/day1/day1.test.ts
+++ src/day1/day1.test.ts
@@ -3,19 +3,25 @@ import { findMultipleOfNumbersThatSumToTargetNumber } from './index';
 describe('Day 1', () => {
   it('should find two elements in the input array which sum to 2020 and multiply them together', () => {
     const inputs = ['1010', '1010', '23', '234', '2345'];
-    const output = findMultipleOfNumbersThatSumToTargetNumber(inputs, 2020);
+    const output = findMultipleOfNumbersThatSumToTargetNumber(inputs, 2020, 2);
     expect(output).toBe(1010 * 1010);
+  });
+
+  it('should find three elements in the input array which sum to 2020 and multiply them together', () => {
+    const inputs = ['10', '2000', '10', '234', '2345'];
+    const output = findMultipleOfNumbersThatSumToTargetNumber(inputs, 2020, 3);
+    expect(output).toBe(10 * 10 * 2000);
   });
 
   it('should return undefined for an input array which does not contain elements which sum to 2020', () => {
     const inputs = ['1', '123', '1234', '12345'];
-    const output = findMultipleOfNumbersThatSumToTargetNumber(inputs, 2020);
+    const output = findMultipleOfNumbersThatSumToTargetNumber(inputs, 2020, 2);
     expect(output).toBe(undefined);
   });
 
   it('should return undefined for an empty input array', () => {
     const inputs: any[] = [];
-    const output = findMultipleOfNumbersThatSumToTargetNumber(inputs, 2020);
+    const output = findMultipleOfNumbersThatSumToTargetNumber(inputs, 2020, 2);
     expect(output).toBe(undefined);
   });
 });
--- src/day1/index.ts
+++ src/day1/index.ts
@@ -2,48 +2,78 @@ import inputs from './inputs.json';
 
 // Advent of Code 2020 - Day 1
 // A program to find two numbers in a list which sum to a given target number
-// Given that a solution definitely exists
 
 /**
  * Finds two numbers in an input array which sum to a target integer
  * @param {string[]} inputs The input array
  * @param {number} targetNumber The target integer
+ * @param {number} numberOfAdditions The desired number of numbers adding up to the target integer
+ * i.e. if numberOfAdditions is 2 then there must be 2 numbers adding up to the target number
  * @return {number[] | undefined} The multiple of the two numbers in the input array which sum to the target integer
  * or undefined if there are no such numbers
  */
 export function findMultipleOfNumbersThatSumToTargetNumber(
   inputs: string[],
-  targetNumber: number
+  targetNumber: number,
+  numberOfAdditions: number
 ): number | undefined {
-  const numbersThatSumTo2020: number[] | undefined = findNumbers(inputs, 2020);
-  const multiple: number | undefined = multiplyNumbers(numbersThatSumTo2020);
+  const numbersThatSumToTarget: number[] | undefined = findNumbers(
+    inputs,
+    targetNumber,
+    numberOfAdditions
+  );
+  const multiple: number | undefined = multiplyNumbers(numbersThatSumToTarget);
   return multiple;
 }
 
 /**
  * Finds two numbers in an input array which sum to a target integer
+ * If a solution with three numbers exists, the function returns three
+ * numbers which sum to the target integer instead
  * @param {string[]} inputs The input array
  * @param {number} targetNumber The target integer
+ * @param {number} numberOfAdditions The desired number of numbers adding up to the target integer
+ * i.e. if numberOfAdditions is 2 then there must be 2 numbers adding up to the target number
  * @return {number[]} The two numbers which sum to the target integer.
  */
 export function findNumbers(
   inputs: string[],
-  targetNumber: number
+  targetNumber: number,
+  numberOfAdditions: number
 ): number[] | undefined {
+  // TODO - this function can probably be made extensible to N entries via recursion
+  let output: number[] = [];
+
   for (let i = 0; i < inputs.length; i++) {
     const firstEntry: number = +inputs[i];
 
     for (let j = i; j < inputs.length; j++) {
       const secondEntry: number = +inputs[j];
+
       if (targetNumber === firstEntry + secondEntry) {
-        console.log([firstEntry, secondEntry]);
-        return [firstEntry, secondEntry];
+        output = [firstEntry, secondEntry];
+        console.log(output);
+        if (numberOfAdditions === 2) {
+          return output;
+        }
+      } else {
+        for (let k = j; k < inputs.length; k++) {
+          const thirdEntry: number = +inputs[k];
+
+          if (targetNumber === firstEntry + secondEntry + thirdEntry) {
+            output = [firstEntry, secondEntry, thirdEntry];
+            console.log(output);
+            if (numberOfAdditions === 3) {
+              return output;
+            }
+          }
+        }
       }
     }
   }
 
   console.warn(
-    'There are no two integers which sum to the target number in the input array'
+    'There are no two or three integers which sum to the target number in the input array'
   );
   return undefined;
 }
@@ -57,6 +87,10 @@ export function multiplyNumbers(
   inputs: number[] | undefined
 ): number | undefined {
   if (inputs) {
-    return inputs[0] * inputs[1];
+    let output: number = 1;
+    for (let i = 0; i < inputs.length; i++) {
+      output *= inputs[i];
+    }
+    return output;
   }
 }
